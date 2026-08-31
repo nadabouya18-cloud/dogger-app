6d9447c35b52a5f923aef2e9af67ba9b6cb0c23e
nadabouya18-cloud <nada.bouya18@gmail.com>
2026-08-31 15:46:05 +0200

    Add RegisterWalker route and protect WalkerHome

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import RegisterWalker from './pages/RegisterWalker';
 import Dashboard from './pages/Dashboard';
 import BookingFlow from './pages/BookingFlow';
 import WalkerHome from './pages/WalkerHome';
@@ -21,7 +22,7 @@ function ProtectedRoute({ children }) {
       <div style={{ fontSize: 48 }}>🐾</div>
     </div>
   );
-  if (!session) return <Navigate to="/login" />;
+  if (!session) return <Navigate to={window.location.pathname === '/walker' ? '/login?redirect=walker' : '/login'} />;
   return children;
 }
 
@@ -32,11 +33,12 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/register-walker" element={<RegisterWalker />} />
         <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="/book" element={<ProtectedRoute><BookingFlow /></ProtectedRoute>} />
         <Route path="/book/:flowType" element={<ProtectedRoute><BookingFlow /></ProtectedRoute>} />
         <Route path="/book/:flowType/:step" element={<ProtectedRoute><BookingFlow /></ProtectedRoute>} />
-        <Route path="/walker" element={<WalkerHome />} />
+        <Route path="/walker" element={<ProtectedRoute><WalkerHome /></ProtectedRoute>} />
         <Route path="/add-dog" element={<ProtectedRoute><AddDog /></ProtectedRoute>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
